1089aeaa0df56d266666147a9e4a8511a3cb7c42
SonOfMagic <qq1324318532@gmail.com>
2022-08-05 13:18:07 +0800

    feat: add app build issue

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ if (process.env.NODE_ENV === "development") {
   process.env.TAILWIND_MODE = "watch";
 }
 
+const isH5 = process.env.UNI_PLATFORM === "h5";
+const isApp = process.env.UNI_PLATFORM === "app";
+
+const WeappTailwindcssDisabled = isH5 || isApp;
+
 const {
   UniAppWeappTailwindcssWebpackPluginV4,
 } = require("weapp-tailwindcss-webpack-plugin");
@@ -12,7 +17,11 @@ const {
 const config = {
   //....
   configureWebpack: {
-    plugins: [new UniAppWeappTailwindcssWebpackPluginV4()],
+    plugins: [
+      new UniAppWeappTailwindcssWebpackPluginV4({
+        disabled: WeappTailwindcssDisabled,
+      }),
+    ],
   },
   //....
 };
