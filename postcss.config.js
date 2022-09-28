03804ba1efaffe73f30d47db693a28bcc583b923
SonOfMagic <qq1324318532@gmail.com>
2022-09-28 10:41:05 +0800

    feat: add @dcloudio/vue-cli-plugin-uni/packages/postcss for h5

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,26 @@
 // Error: ENOENT: no such file or directory, open 'D:\HBuilderX\plugins\uniapp-cli\tailwind.config.js'
 
 const path = require("path");
-const { WeappTailwindcssDisabled } = require("./platform");
+const {
+	WeappTailwindcssDisabled
+} = require("./platform");
 
 module.exports = {
-  plugins: [
-    require("autoprefixer")({
-      remove: process.env.UNI_PLATFORM !== "h5",
-    }),
-    require("tailwindcss")({
-      config: path.resolve(__dirname, "./tailwind.config.js"),
-    }),
-    // rem 转 rpx
-    WeappTailwindcssDisabled
-      ? undefined
-      : require("postcss-rem-to-responsive-pixel/postcss7")({
-          rootValue: 32,
-          propList: ["*"],
-          transformUnit: "rpx",
-        }),
-  ],
+	plugins: [
+		require("autoprefixer")({
+			remove: process.env.UNI_PLATFORM !== "h5",
+		}),
+		require("tailwindcss")({
+			config: path.resolve(__dirname, "./tailwind.config.js"),
+		}),
+		// rem 转 rpx
+		WeappTailwindcssDisabled ?
+		undefined :
+		require("postcss-rem-to-responsive-pixel/postcss7")({
+			rootValue: 32,
+			propList: ["*"],
+			transformUnit: "rpx",
+		}),
+		WeappTailwindcssDisabled ? require('@dcloudio/vue-cli-plugin-uni/packages/postcss') : undefined
+	],
 };
