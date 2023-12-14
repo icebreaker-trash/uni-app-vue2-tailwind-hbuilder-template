58bf79532f19a9057b7e5a41656d9134e34788d3
ice breaker <1324318532@qq.com>
2023-12-14 22:08:21 +0800

    chore: commit codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 // tailwindcss-miniprogram-preset 适合那种 webpack 无法触及的场景
-const path = require("path");
-const resolve = (p) => {
-  return path.resolve(__dirname, p);
-};
+const { resolve } = require('./platform')
 // 基础配置，无需任何preset
 // https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/blob/main/demo/uni-app/tailwind.config.js
 /** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
