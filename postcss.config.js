85e13eb76731b268c30467aa122192023e8ff789
ice breaker <1324318532@qq.com>
2025-06-05 13:59:55 +0800

    fix: 修复 rpx -> px

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,18 @@ const {
 
 module.exports = {
 	plugins: [
+		require("postcss-import")({
+			resolve(id, basedir, importOptions) {
+				if (id.startsWith("~@/")) {
+					return path.resolve(process.env.UNI_INPUT_DIR, id.substr(3));
+				} else if (id.startsWith("@/")) {
+					return path.resolve(process.env.UNI_INPUT_DIR, id.substr(2));
+				} else if (id.startsWith("/") && !id.startsWith("//")) {
+					return path.resolve(process.env.UNI_INPUT_DIR, id.substr(1));
+				}
+				return id;
+			},
+		}),
 		require("autoprefixer")({
 			remove: process.env.UNI_PLATFORM !== "h5",
 		}),
